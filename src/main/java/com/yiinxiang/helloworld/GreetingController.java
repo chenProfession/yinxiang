a601f16c2329a672a70bcb92fc86eed5c226a368
Cheng <ccelephant2012@gmail.com>
2019-03-24 14:43:30 +0800

    add logback configeration

--- src/main/java/com/yiinxiang/helloworld/GreetingController.java
+++ src/main/java/com/yiinxiang/helloworld/GreetingController.java
@@ -1,5 +1,6 @@
 package com.yiinxiang.helloworld;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 
 @RestController
+@Slf4j
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -21,6 +23,11 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        log.trace("trace the log");
+        log.debug("debug the log");
+        log.info("info the log");
+        log.warn("warn the log");
+        log.error("error the log");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
